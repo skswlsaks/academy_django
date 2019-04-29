1a7c66c6c1feb339cbd96b26c36e20d6cc5e268d
Jin Park <skswlsaks@gmail.com>
2019-04-29 23:20:03 +0900

    ignore added, onmessage need to be done

--- signaling/consumers.py
+++ signaling/consumers.py
@@ -1,11 +1,11 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
+# from channels.consumer import AsyncConsumer
 import json
 
 class SignalConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name,
@@ -24,21 +24,35 @@ class SignalConsumer(AsyncWebsocketConsumer):
     # Receive message from WebSocket
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
+        text_data_json = text_data_json['msg']
+        print(text_data_json)
+        
+        if ('holy' in text_data_json.keys()):
+            key = 'holy'
+        elif ('info' in text_data_json.keys()):
+            key = 'info'
+        
+        message = text_data_json[key]
         await self.channel_layer.group_send(
             self.room_group_name,
             {
-                'type': 'message',
+                'type': key,
                 'message': message
             }
         )
 
-    # Receive message from room group
-    async def chat_message(self, event):
-        message = event['message']
+        # Send message to room group
 
+    # Receive message from room group
+    async def holy(self, event):
+        message = event['message']
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+    async def info(self, event):
+        message = event['message']
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message
--- signaling/routing.py
+++ signaling/routing.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import consumers 
 
 websocket_urlpatterns = [
-	url(r'^ws/signaling[^/]+)/$', consumers.SignalConsumer),
+	url(r'^ws/signaling/(?P<room_name>[^/]+)/$', consumers.SignalConsumer),
 ]
