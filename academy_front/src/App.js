4d726cac47ce1683b09b8b1d9b685ebed4522aa2
Jin Park <skswlsaks@gmail.com>
2019-05-29 00:11:59 +0900

    mainform working

--- academy_front/src/App.js
+++ academy_front/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MainForm from './containers/MainView';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Route, Switch } from 'react-router-dom';
@@ -10,8 +11,8 @@ function App() {
         <div className="App">
             <Router>
                 <Switch>
-                    <Route path="/" exact component={Main} />
-                    <Route path="/Login/" component={Login} />
+                    <Route path="/" exact component={MainForm} />
+                    {/* <Route path="/Login/" component={Login} /> */}
                 </Switch>
             </Router>
         </div>
--- academy_front/src/containers/MainView.js
+++ academy_front/src/containers/MainView.js
@@ -6,18 +6,23 @@ class MainForm extends React.Component {
 
 	render () {
 		return (
-			
+			<div>
 			<h1>Realtime communication with WebRTC</h1>
 			<div id="videos">
-				<video id="localVideo" autoplay playsinline />
-				<video id="remoteVideo" autoplay playsinline />
-				<audio id="audio" autoplay />
+				<video id="localVideo" autoPlay playsInline />
+				<video id="remoteVideo" autoPlay playsInline />
+				<audio id="audio" autoPlay />
 			</div>
 
 			<div>
 				<button id="send">Send</button>
-				<button id
+				<button id="get_screen">Get screen</button>
+				<button id="call">Call</button>
+				<button id="sendData">Data send</button>
+			</div>
 			</div>
 		);
 	}
 }
+
+export default MainForm;
