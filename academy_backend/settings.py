73115a187a94418d2dbb78a38306fe9342158793
Brian Min <bymi15@yahoo.com>
2019-06-28 17:02:58 +0900

    Update settings.py
    use redis docker container

--- academy_backend/settings.py
+++ academy_backend/settings.py
@@ -168,7 +168,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("redis", 6379)],
         },
     }
 }
